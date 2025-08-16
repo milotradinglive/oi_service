c6e8f96d601c42e4e81244cf585de4f59cd2db93
milotradinglive <milo.trading.live@milotradinglive.com>
2025-08-16 13:52:19 -0500

    Update app.py

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ MAIN_FILE_ID   = os.getenv("MAIN_FILE_ID",   "1DlwiPxbgDWAQmM_7n5MRi2Ms4YRas5SYK
 ACCESS_FILE_ID = os.getenv("ACCESS_FILE_ID", "1ZwLVuinFA1sBprPMWVliu_nwdr1mlmav6FJ-zQm2FlE")  # usa otro ID si tu hoja de accesos es distinta
 ACCESS_SHEET_TITLE = "AUTORIZADOS"
 
-TRADIER_TOKEN  = os.getenv("TRADIER_TOKEN",  "REEMPLAZA_CON_TU_TOKEN")  # <- ponlo en env/secret
+TRADIER_TOKEN  = os.getenv("TRADIER_TOKEN",  "LGFG3UgaSG6y6GwoXhrBGKGnK5Vk")  # <- ponlo en env/secret
 
 # ========= Auth desde variable de entorno =========
 def make_gspread_and_creds():
