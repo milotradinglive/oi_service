ecb8b7f160568c361858a102ff7e385b4b66e0c2
milotradinglive <milo.trading.live@milotradinglive.com>
2026-02-03 15:57:11 -0500

    Update app.py

--- app.py
+++ app.py
@@ -98,9 +98,9 @@ drive = build("drive", "v3", credentials=google_api_creds)
 # ========= Datos base =========
 TICKERS = [
     "AAPL","AMD","AMZN","BA","BAC","DIA","GLD","GOOG","IBM","INTC",
-    "IWM","JPM","META","MRNA","MSFT","NFLX","NVDA","NVTS","ORCL","UBER",
-    "PLTR","QQQ","SLV","SNAP","SPY","TNA","TSLA","USO","WFC","WMT","XOM","V",
-    "URA","CVX","PYPL",
+    "IWM","JPM","META","MRNA","MSFT","NFLX","NVDA","ORCL","UBER",
+    "PLTR","QQQ","SLV","SPY","TNA","TSLA","USO","WFC","WMT","XOM",
+    "CVX","PYPL",
 ]
 
 # ========= Tradier =========
